21392b9c3b57a608d0672229a02aac682894703b
unknown <adamgie402@gmail.com>
2022-08-21 21:01:30 +0200

    multiple interval problem solved & default html click action solved

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
+// ***************************************
+// Smooth Scrolling
+// ***************************************
+
 // get htmlcollection which has class smoothScrolling
 let smoothScrollAnchors = document.getElementsByClassName('smoothScrolling');
 
 //  iteration by htmlcollection and adding click event listener to everyone
 for ( let i=0; i < smoothScrollAnchors.length; i++) {
     smoothScrollAnchors[i].addEventListener("click", smoothScroll, false);
-
-    console.log(smoothScrollAnchors[i]);
 }
 
-function smoothScroll() {   
-    // console.log('f smooth scroll...');
-    // console.log(this);
+function smoothScroll(e) {   
 
     // *** setup
     let intervalSpeed = 5; // 1 - faster, 10 - slower
     let extraOffset = 0; // default: 0; this is optional; + and - values are allowed 
     // *** end of setup
+    
+    e.preventDefault(); // prevent default html jump to target place (#target)
+    // console.log(e);    
+    // target anchor of clicked element:
+    // console.log(e.target.hash);
 
+    // Preventing many intervals in this same time:
+    // Chceck for previous interval runing (if interval exist is always a number)
+    // so, if it exist - use clearInterval(intervalName)
+    // (you cannot use clearInterval if interval doesn't exist - error: is not defined)
+    if (typeof scrollInterval == "number") { clearInterval(scrollInterval); }
+
+    // get anchor value (target id on page)
     let href = this.getAttribute('href');        
     let p = href.indexOf("#"); // position of # in href addres
     let targetId = href.slice(p+1); // slice to only name of href (target id) 
@@ -27,25 +39,31 @@ function smoothScroll() {
     
     // console.log('currentY = ' + currentY + ' & targetY = ' + targetY);
 
-    if (currentY < targetY) {            
-        let scroolInterval = setInterval(function() {          
-                window.scrollTo(0, currentY); // start scrolling from current position
-                currentY += 25; // step - speed of movement (jump 200px)
-                if (currentY > targetY) { 
-                    clearInterval(scroolInterval); // stop interval = stop scrolling
-                    window.scrollTo(0,targetY); // to precise position set after scrolling step by step
-                };
+    if (currentY < targetY) {
+
+        scrollInterval = setInterval(function() {          
+            window.scrollTo(0, currentY); // start scrolling from current position
+            currentY += 25; // step - speed of movement (jump 200px)
+            // console.log(scrollInterval);
+            if (currentY > targetY) { 
+                clearInterval(scrollInterval); // stop interval = stop scrolling
+                window.scrollTo(0,targetY); // to precise position set after scrolling step by step
+            };
         }, intervalSpeed);
     }
     
     if (currentY > targetY) {            
-        let scroolInterval = setInterval(function() {          
-                window.scrollTo(0, currentY); // start scrolling from current position
-                currentY -= 25; // step - speed of movement (jump 200px)
-                if (currentY < targetY) { 
-                    clearInterval(scroolInterval); 
-                    window.scrollTo(0,targetY); // to precise position set after scrolling step by step
-                };
+
+        scrollInterval = setInterval(function() {          
+            window.scrollTo(0, currentY); // start scrolling from current position
+            currentY -= 25; // move step-by-step by this value (25px give smooth scrooling)
+            // console.log(scrollInterval);
+            if (currentY < targetY) { 
+                clearInterval(scrollInterval); 
+                window.scrollTo(0,targetY); // to precise position set after scrolling step by step
+            };
         }, intervalSpeed);
     }
 }
+
+// ***************************************
