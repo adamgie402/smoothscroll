bb359b00b2c275628d6b5da67d55d08481b5abb6
unknown <adamgie402@gmail.com>
2022-08-21 19:31:02 +0200

    js smooth scrolling function

--- script.js
+++ script.js
@@ -8,4 +8,44 @@ for ( let i=0; i < smoothScrollAnchors.length; i++) {
     console.log(smoothScrollAnchors[i]);
 }
 
-function smoothScroll() {}
+function smoothScroll() {   
+    // console.log('f smooth scroll...');
+    // console.log(this);
+
+    // *** setup
+    let intervalSpeed = 5; // 1 - faster, 10 - slower
+    let extraOffset = 0; // default: 0; this is optional; + and - values are allowed 
+    // *** end of setup
+
+    let href = this.getAttribute('href');        
+    let p = href.indexOf("#"); // position of # in href addres
+    let targetId = href.slice(p+1); // slice to only name of href (target id) 
+
+    let targetY = document.getElementById(targetId).offsetTop; // get Y position of target anchor
+    targetY = targetY + extraOffset;
+    let currentY = window.scrollY; // current scroll Y position
+    
+    // console.log('currentY = ' + currentY + ' & targetY = ' + targetY);
+
+    if (currentY < targetY) {            
+        let scroolInterval = setInterval(function() {          
+                window.scrollTo(0, currentY); // start scrolling from current position
+                currentY += 25; // step - speed of movement (jump 200px)
+                if (currentY > targetY) { 
+                    clearInterval(scroolInterval); // stop interval = stop scrolling
+                    window.scrollTo(0,targetY); // to precise position set after scrolling step by step
+                };
+        }, intervalSpeed);
+    }
+    
+    if (currentY > targetY) {            
+        let scroolInterval = setInterval(function() {          
+                window.scrollTo(0, currentY); // start scrolling from current position
+                currentY -= 25; // step - speed of movement (jump 200px)
+                if (currentY < targetY) { 
+                    clearInterval(scroolInterval); 
+                    window.scrollTo(0,targetY); // to precise position set after scrolling step by step
+                };
+        }, intervalSpeed);
+    }
+}
